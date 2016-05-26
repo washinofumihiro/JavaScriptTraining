4ec7794c2d364138b091094523e52d952a7b93ed
washinofumihiro <washino.fumihiro@tis.co.jp>
2016-05-27 08:46:30 +0900

    add JavaScript task stage.6

--- public/stage6/sample/script.js
+++ public/stage6/sample/script.js
@@ -1,2 +1,48 @@
 // ボタンはサービスです。
 // 自由に使ってください。
+	  //var element = document.getElementsByClassName('.button button-caution button-3d button-pill button-giant js-training-button');
+	  //var element = document.querySelector('button');
+	  
+	  //var $element = $('.button button-caution button-3d button-pill button-giant js-training-button');
+	  //$element.css({'opacity' : '0.5'});
+	  //element.addEventListener('click',function(){
+	  //	element.textContent = Number(element.textContent) + 1;
+	  //	});
+
+	  //var $element = $('button');
+	  //var selector = '.js-training-playground:nth-child(1) button';
+	  //selector.append
+	  
+	  
+	  //おさかな追加
+	  //var elementToAdd = document.createElement('span');
+      //elementToAdd.textContent = '\uD83D\uDC1F';
+      //var element = document.querySelector('button');
+      //element.insertBefore(elementToAdd,element.firstChild);
+      
+      
+      //$(.button').remove();
+      
+      //jqueryの場合
+      //var $elementToAdd = $('<span>\uD83D\uDC1F</span>');
+	  //$('button').append($elementToAdd);
+
+	  //色の変更
+	  var $element = $('.button')[0];
+      
+      $(element).css('background-color','green');
+
+	  //回転
+      //var num = 0;
+	  //$('.bottun').on('click', function(event) {
+	  //	var $target = $(event.target);
+	  //	num += 10;
+	  //	//$target.text(Number(num) - 1);
+	  	//$target.text(Number($target.text()) - 2);
+	 // 	$target.css({ transform : 'rotate('+ num +'deg)'});
+	 // 	});
+	  
+
+ 	  //var $element = $('button');
+	  //$element.css({'opacity' : '0.5'});
+      
--- public/stage6/tests.js
+++ public/stage6/tests.js
@@ -4,7 +4,32 @@ describe('ステージ6（意図通りにモジュールを書ける）', functi
   it('あなたの満足のいく Web アプリケーションがここにある', function() {
     // ボタンはサービスです。
     // 自由に使ってください。
+	
+	  //var element = document.getElementsByClassName('.button button-caution button-3d button-pill button-giant js-training-button');
+	  //var element = document.querySelector('button');
+	  //element
+	  //elementToAdd.textContent = '\uD83D\uDC2C';
+	  
+	  //var $element = $('.button button-caution button-3d button-pill button-giant js-training-button');
+	  //$element.css({'opacity' : '0.5'});
+	  //element.addEventListener('click',function(){
+	  //	element.textContent = Number(element.textContent) + 1;
+	  //	});
 
+	  //var $element = $('button');
+	  var selector = '.js-training-playground:nth-child(1) button';
+	  selector.append
+	  
+	  
+	  
+	  var elementToAdd = document.createElement('span');
+      elementToAdd.textContent = '\uD83D\uDC1F';
+
+      // 上の elementToAdd を、5 番の青色の要素の最初に追加するコードを
+      // ここに記述してください。
+      var element = document.querySelector('button');
+      element.insertBefore(elementToAdd,element.firstChild);
+	  
     var qualityOfYourAppliation = undefined;
 
     expect(qualityOfYourAppliation).to.be.ok;
